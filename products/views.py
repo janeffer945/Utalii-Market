b4ba5666bc2898a9e352b3b91bc5b93ac3909cb4
janeffer945 <janeffernjeri945@gmail.com>
2025-05-09 22:38:16 +0300

    rest_framework

--- products/views.py
+++ products/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import *
+from django.db.models import Q
+from collections import defaultdict
 
 # Create your views here.
