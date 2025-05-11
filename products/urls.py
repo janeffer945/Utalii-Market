76d3e619d5658244df003dba05a76432aaf7b9ec
janeffer945 <janeffernjeri945@gmail.com>
2025-05-11 12:34:13 +0300

    reproducts view records

--- products/urls.py
+++ products/urls.py
@@ -3,4 +3,6 @@ from products import views
 
 urlpatterns = [
     path('recommend/', views.RecommendProductsView.as_view(), name='recommend_products'),
+    path('history/', views.BrowsingHistoryView.as_view(), name='browsing_history'),
+    path('view-product/', views.ViewProduct.as_view(), name='view_product'),
 ]
